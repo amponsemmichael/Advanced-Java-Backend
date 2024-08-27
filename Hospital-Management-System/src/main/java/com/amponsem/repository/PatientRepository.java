453c04ac42b56f792148c66f49a429874b9f3236
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-27 11:52:56 +0000

    Function: add repo for varoius entities

---
+++ Hospital-Management-System/src/main/java/com/amponsem/repository/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.amponsem.repository;
+
+import com.amponsem.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {}
